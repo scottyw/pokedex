324fd84d880e31c321dccb3dd079ec32464ac290
Scott Walker <scott.walker@puppet.com>
2019-04-15 15:21:01 +0100

    Giant table

--- cmd/pokedex/main.go
+++ cmd/pokedex/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -26,14 +27,14 @@ type pokemon struct {
 	} `json:"base"`
 }
 
-var ps []pokemon
+var all []pokemon
 
 func init() {
 	data, err := ioutil.ReadFile("data/pokemon.json")
 	if err != nil {
 		panic(err)
 	}
-	err = json.Unmarshal(data, &ps)
+	err = json.Unmarshal(data, &all)
 	if err != nil {
 		panic(err)
 	}
@@ -42,12 +43,30 @@ func init() {
 func main() {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, ps)
+		http.Redirect(w, r, "/dex", http.StatusFound)
 	})
-	http.HandleFunc("/action", func(w http.ResponseWriter, r *http.Request) {
-		ps = ps[1:]
-		http.Redirect(w, r, "/", http.StatusFound)
+	http.HandleFunc("/dex", func(w http.ResponseWriter, r *http.Request) {
+		filter := r.URL.Query().Get("filter")
+		if filter != "" {
+			var ps []pokemon
+			for _, p := range all {
+				if strings.Contains(strings.ToLower(p.Name.English), strings.ToLower(filter)) {
+					ps = append(ps, p)
+				}
+			}
+			tmpl.Execute(w, ps)
+		} else {
+			tmpl.Execute(w, all)
+		}
 	})
-	fmt.Println("Serving http://localhost:8080/")
+	// http.HandleFunc("/action", func(w http.ResponseWriter, r *http.Request) {
+	// 	bs, err := ioutil.ReadAll(r.Body)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	filter := string(bs)
+	// 	http.Redirect(w, r, fmt.Sprintf("/dex?filter=%s", filter), http.StatusFound)
+	// })
+	fmt.Println("Serving http://localhost:8080/dex")
 	http.ListenAndServe(":8080", nil)
 }
